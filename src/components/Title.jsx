f4c812d25cd1d795ecd1d6a6382faf07043d3acd
merkoyep <81783308+merkoyep@users.noreply.github.com>
2024-02-06 12:36:42 -0800

    added title

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Title.css';
+
+const Title = () => {
+  return (
+    <div className='Title'>
+      <h1>Parks and Rec Asset Map</h1>
+    </div>
+  );
+};
+
+export default Title;
